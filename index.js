1cfe0bd53746e937ea38067a15eb874e73d3c078
Fireblade75 <Fireblade75@users.noreply.github.com>
2018-05-15 09:30:58 +0200

    index

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const server = express()
 const port = process.env.PORT | 3000
 process.env.DEBUG = 'dialogflow:debug'
 
-server.use((req, res, next) => {
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+server.post('/bot', (req, res) => {
     const agent = new WebhookClient({req, res})
     console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers))
     console.log('Dialogflow Request body: ' + JSON.stringify(request.body))
@@ -26,4 +30,6 @@ function fallback(agent) {
     agent.add(`I'm sorry, can you try again?`);
 }
 
-server.listen(port)
+server.listen(port, () => {
+    console.log("listening on port " + port)
+})
